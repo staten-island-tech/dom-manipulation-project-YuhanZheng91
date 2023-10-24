59adb5a9489ac315f2319dad4e04def0dd79086a
yuhanz1 <yuhanz9149@gmail.com>
2023-10-24 10:13:37 -0400

    qq

--- dom.js
+++ dom.js
@@ -1,16 +1,31 @@
 const DOMSelectors = {
     form: document.querySelector("#formm"),
-    something: document.querySelector(".smth"),
     h2s: document.querySelectorAll("h2"),
+    picture: document.querySelector(".pic"),
+    something: document.querySelector(".smth"),
 };
 
+//make album
 DOMSelectors.form.addEventListener("submit", function(event){
     event.preventDefault();
-    console.log(DOMSelectors.smth.value);
-    DOMSelectors.h2s.forEach((el)=> el.textContent = DOMSelectors.smth);
+    console.log(DOMSelectors.something.value)
+    DOMSelectors.h2s.forEach((el)=> el.textContent = DOMSelectors.something.value);
+    //DOMSelectors.picture.forEach((el)=> el.textContent = DOMSelectors.image.value);
 });
 
-//makealbum
 //add card
-//
-//adddeletecard functionthng
+DOMSelectors.form.addEventListener("asdjksfshfkj", function(event){
+    event.preventDefault();
+});
+
+//clearflieds
+//DOMSelectors.form.addEventListener();
+
+//add delete button
+//DOMSelectors.form.addEventListener();
+
+
+//makealbum
+//add card: insertadjcenthtml
+//clearFields() -- make x === ""
+//adddeletecard functionthing queryselectortoallandaddevent ;foreach loop
