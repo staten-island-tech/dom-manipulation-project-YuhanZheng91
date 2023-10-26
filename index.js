01caffc0351e68c84abcb5a46d2e9ade158aff90
yuhanz1 <yuhanz9149@gmail.com>
2023-10-26 10:15:31 -0400

    1

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+//restarted:
+document.body.style.backgroundColor = "#fad6a5";
+document.form.style.color ="#a0785a";
+document.getElementById("h2").style.color = "#a0785a";
+
 const DOMSelectors = {
     form: document.querySelector("#formm"),
     ent: document.querySelector(".entertain"),
     creator: document.querySelector(".enterFavCreator"),
 };
 
+//object and calls following functions
 DOMSelectors.form.addEventListener("submit", function(event) {
     event.preventDefault();
 
@@ -12,15 +18,19 @@ DOMSelectors.form.addEventListener("submit", function(event) {
     const creator = DOMSelectors.creator.value;
     
     // Create an album object
-    const album = makeCard(title, creator, ""); 
+    const card = makeCard(title, creator, ""); 
 
     // Add the album card to the DOM
-    addCard(album);
+    addCard(card);
 
     // Clear input fields
     clearFields();
+
+    // Delete buttons
+
 });
 
+//make cards
 function makeCard(title, creator, imageURL) {
     return {
         title: title,
@@ -29,21 +39,27 @@ function makeCard(title, creator, imageURL) {
     };
 }
 
+//add card
 function addCard(card) {
     const cardHTML = `
         <div class="container">
             <h2 class="titleOfEnt">${card.title}</h2>
-            <h3 class="creatorOfEnt">${card.creator}</h3>
-            <img class="pic" src="${card.imageURL}" alt="your-inserted-image">
+            <h2 class="creatorOfEnt">${card.creator}</h2>
+            <img class="pic" src="${card.imageURL}" alt="image">
             <button class="delete-button">Delete</button>
         </div>
     `;
 
-    DOMSelectors.form.insertAdjacentHTML("beforeend", cardHTML);
+    DOMSelectors.form.insertAdjacentHTML("afterend", cardHTML);
 }
 
+//clears the input fields
 function clearFields() {
     DOMSelectors.ent.value = "";
     DOMSelectors.creator.value = "";
 }
 
+//removes an object
+function clickDelete(){
+
+}
